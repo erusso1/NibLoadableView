57f9c10944b5d888c6cc019fb6c124eb40412d0f
Ephraim Russo <ephraim.s.russo@gmail.com>
2019-04-05 15:11:13 -0700

    Initial commit

---
+++ Example/NibLoadableView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  NibLoadableView
+//
+//  Created by erusso1 on 04/05/2019.
+//  Copyright (c) 2019 erusso1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
